0120b0770ca1d1e8b5882a2869cce276e0d63f09
Matthias <m@h.de>
2017-01-01 02:50:48 +0100

    message added

--- MyProject/src/com/project/my/Main2.java
+++ MyProject/src/com/project/my/Main2.java
@@ -3,7 +3,7 @@ package com.project.my;
 public class Main2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! This is a test.");
 	}
 
 }
