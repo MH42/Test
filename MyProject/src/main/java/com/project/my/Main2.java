028a21b0b3a7de4f091befacfce0217580bf5cb1
Matthias <m@h.de>
2017-02-26 01:19:39 +0100

    Trigger3

--- MyProject/src/main/java/com/project/my/Main2.java
+++ MyProject/src/main/java/com/project/my/Main2.java
@@ -3,7 +3,7 @@ package com.project.my;
 public class Main2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!! This is a test!!");
+		System.out.println("Hello World!! This is a test!!!");
 	}
 
 }
